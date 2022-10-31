e064db9b50bba663deb1cee2b7c223bdbb2b142c
MykhailoHordiienko <gordienkomichael191@gmail.com>
2022-10-31 23:34:51 +0000

    Deploying to gh-pages from @ MykhailoHordiienko/Filmoteka@d6a4e00c5bc94e53283c2b6203210f354c2acfef 🚀

--- index.686ebe67.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open-team]"),closeModalBtn:document.querySelector("[data-modal-close-team]"),modal:document.querySelector("[data-modal-team]")};function t(){event.preventDefault(),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.686ebe67.js.map
