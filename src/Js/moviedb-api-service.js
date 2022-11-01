17f4d6f681e236115b1728b9b14dc54420bd5b67
Alexey-Kushnir <al7787@gmail.com>
2022-11-01 21:02:39 +0200

    modal-one -movie

---
+++ src/Js/moviedb-api-service.js
@@ -0,0 +1,29 @@
+const BASE_URL = 'https://api.themoviedb.org/';
+const API_KEY = '1f93214cb1bbadcc143eeb01d552ab8c';
+
+export default class CountriesApiService {
+  constructor() {
+    this.id = '';
+  }
+
+  fetchMovie() {
+    const url = `${BASE_URL}${this.id}/movie/550?api_key=${API_KEY}`;
+
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          error => console.log(error);
+        }
+        return response.json();
+      })
+      .catch(error => console.log(error));
+  }
+
+  get query() {
+    return this.id;
+  }
+
+  set query(newQuery) {
+    this.id = newQuery;
+  }
+}
