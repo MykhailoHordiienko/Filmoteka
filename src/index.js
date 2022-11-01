a29c5c6a07e9afbd0fa7392b774a6205523ae864
Alex Vainahii <104431359+AlexVainahii@users.noreply.github.com>
2022-11-01 19:35:22 +0200

    іі

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './Js/modal-team-info.js';
-import './Js/button';
-import './Js/pagination';
+// import './Js/button';
+// import './Js/pagination';
 import './Js/informing';
 import './Js/apiService';
 import './Js/gallery';
 import './Js/spinner';
 import './Js/auth';
-import './Js/buttons';
+//import './Js/buttons';
 import './Js/refs';
 
 
